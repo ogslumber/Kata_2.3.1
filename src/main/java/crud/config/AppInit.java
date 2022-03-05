e22e4217e237a0d8d46d7a6fda55781d9a6de946
ogslumber <olivewarrior8@gmail.com>
2022-03-05 16:50:10 +0500

    Фикс отображения кириллицы

--- src/main/java/crud/config/AppInit.java
+++ src/main/java/crud/config/AppInit.java
@@ -1,8 +1,10 @@
 package crud.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
@@ -28,6 +30,12 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
     @Override
     public void onStartup(ServletContext aServletContext) throws ServletException {
         super.onStartup(aServletContext);
+
+        FilterRegistration.Dynamic encodingFilter = aServletContext.addFilter("encodingFilter", new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
+
         registerHiddenFieldFilter(aServletContext);
     }
 
